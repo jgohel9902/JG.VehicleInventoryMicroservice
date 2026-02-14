0fbdfeeed8cdeecba0becf000f978d9678214bd0
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 13:16:19 -0500

    feat(domain): add domain rule violation exception

---
+++ VehicleInventory.Domain/Exceptions/DomainRuleViolationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory.Domain.Exceptions
+{
+    public sealed class DomainRuleViolationException : Exception
+    {
+        public DomainRuleViolationException(string message) : base(message)
+        {
+        }
+    }
+}
