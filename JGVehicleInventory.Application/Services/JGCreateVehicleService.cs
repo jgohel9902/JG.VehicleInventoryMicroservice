e27ed997a3bc9fcb5cfb27f30982783f70a7f42c
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 18:50:56 -0500

    feat(application): implement JGCreateVehicleService use case

--- JGVehicleInventory.Application/Services/JGCreateVehicleService.cs
+++ JGVehicleInventory.Application/Services/JGCreateVehicleService.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JGVehicleInventory.Application.DTOs;
+using JGVehicleInventory.Application.Interfaces;
+using VehicleInventory.Domain.Entities;
 
-namespace JGVehicleInventory.Application.Services
+namespace JGVehicleInventory.Application.Services;
+
+public sealed class JGCreateVehicleService
 {
-    internal class JGCreateVehicleService
+    private readonly JGIVehicleRepository _repository;
+
+    public JGCreateVehicleService(JGIVehicleRepository repository)
     {
+        _repository = repository;
+    }
+
+    public async Task<JGVehicleResponseDto> ExecuteAsync(
+        JGCreateVehicleRequestDto request,
+        CancellationToken cancellationToken = default)
+    {
+        if (await _repository.ExistsByVehicleCodeAsync(request.VehicleCode, cancellationToken))
+            throw new InvalidOperationException("Vehicle with same code already exists.");
+
+        var vehicle = new Vehicle(
+            Guid.NewGuid(),
+            request.VehicleCode,
+            request.LocationId,
+            request.VehicleType
+        );
+
+        await _repository.AddAsync(vehicle, cancellationToken);
+
+        return new JGVehicleResponseDto
+        {
+            Id = vehicle.Id,
+            VehicleCode = vehicle.VehicleCode,
+            LocationId = vehicle.LocationId,
+            VehicleType = vehicle.VehicleType,
+            Status = vehicle.Status
+        };
     }
 }
