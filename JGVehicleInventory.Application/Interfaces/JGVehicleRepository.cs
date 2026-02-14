4b0fcc2b61e999697a06084bf64243b05122839c
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 18:32:39 -0500

    feat(application): add JGIVehicleRepository contract

---
+++ JGVehicleInventory.Application/Interfaces/JGVehicleRepository.cs
@@ -0,0 +1,17 @@
+﻿using VehicleInventory.Domain.Entities;
+
+namespace JGVehicleInventory.Application.Interfaces
+{
+    public interface JGVehicleRepository
+    {
+        Task<Vehicle?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<Vehicle>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task AddAsync(Vehicle vehicle, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Vehicle vehicle, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Vehicle vehicle, CancellationToken cancellationToken = default);
+
+        Task<bool> ExistsByVehicleCodeAsync(string vehicleCode, CancellationToken cancellationToken = default);
+
+    }
+}
