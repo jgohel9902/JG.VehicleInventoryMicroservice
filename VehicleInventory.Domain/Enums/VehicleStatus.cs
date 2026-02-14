e8ba650b88d80eaaa21376c49428acc4e37f5375
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 13:14:10 -0500

    feat(domain): add VehicleStatus enum

---
+++ VehicleInventory.Domain/Enums/VehicleStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventory.Domain.Enums
+{
+    public enum VehicleStatus
+    {
+        Available = 1,
+        Reserved = 2,
+        Rented = 3,
+        Serviced = 4
+    }
+}
