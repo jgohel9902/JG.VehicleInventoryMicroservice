4b68c17eb141fe1ec100a9ad31a96e2744a2c24e
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 18:51:30 -0500

    feat(application): implement JGGetVehicleByIdService use case

--- JGVehicleInventory.Application/Services/JGGetVehicleByIdService.cs
+++ JGVehicleInventory.Application/Services/JGGetVehicleByIdService.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using JGVehicleInventory.Application.DTOs;
+using JGVehicleInventory.Application.Interfaces;
 
-namespace JGVehicleInventory.Application.Services
+namespace JGVehicleInventory.Application.Services;
+
+public sealed class JGGetVehicleByIdService
 {
-    internal class JGGetVehicleByIdService
+    private readonly JGIVehicleRepository _repository;
+
+    public JGGetVehicleByIdService(JGIVehicleRepository repository)
     {
+        _repository = repository;
+    }
+
+    public async Task<JGVehicleResponseDto?> ExecuteAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        var vehicle = await _repository.GetByIdAsync(id, cancellationToken);
+
+        if (vehicle is null)
+            return null;
+
+        return new JGVehicleResponseDto
+        {
+            Id = vehicle.Id,
+            VehicleCode = vehicle.VehicleCode,
+            LocationId = vehicle.LocationId,
+            VehicleType = vehicle.VehicleType,
+            Status = vehicle.Status
+        };
     }
 }
