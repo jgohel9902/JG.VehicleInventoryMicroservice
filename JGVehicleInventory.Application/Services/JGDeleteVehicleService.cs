b54f3de4d7de6473629eb10e09b07b6c48d399d2
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 19:05:21 -0500

    feat(application): implement JGDeleteVehicleService use case

---
+++ JGVehicleInventory.Application/Services/JGDeleteVehicleService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JGVehicleInventory.Application.Interfaces;
+
+namespace JGVehicleInventory.Application.Services;
+
+public sealed class JGDeleteVehicleService
+{
+    private readonly JGIVehicleRepository _repository;
+
+    public JGDeleteVehicleService(JGIVehicleRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<bool> ExecuteAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        var vehicle = await _repository.GetByIdAsync(id, cancellationToken);
+
+        if (vehicle is null)
+            return false;
+
+        await _repository.DeleteAsync(vehicle, cancellationToken);
+
+        return true;
+    }
+}
