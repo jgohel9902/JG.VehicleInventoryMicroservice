c2e35172f775b0d3c2f34bac2a12496fce573f0f
Jenil Gohel <jgohel9902@gmail.com>
2026-02-14 19:02:34 -0500

    feat(application): implement JGGetAllVehiclesService use case

---
+++ JGVehicleInventory.Application/Services/JGGetAllVehiclesService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JGVehicleInventory.Application.DTOs;
+using JGVehicleInventory.Application.Interfaces;
+
+namespace JGVehicleInventory.Application.Services;
+
+public sealed class JGGetAllVehiclesService
+{
+    private readonly JGIVehicleRepository _repository;
+
+    public JGGetAllVehiclesService(JGIVehicleRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IReadOnlyList<JGVehicleResponseDto>> ExecuteAsync(
+        CancellationToken cancellationToken = default)
+    {
+        var vehicles = await _repository.GetAllAsync(cancellationToken);
+
+        return vehicles
+            .Select(v => new JGVehicleResponseDto
+            {
+                Id = v.Id,
+                VehicleCode = v.VehicleCode,
+                LocationId = v.LocationId,
+                VehicleType = v.VehicleType,
+                Status = v.Status
+            })
+            .ToList();
+    }
+}
