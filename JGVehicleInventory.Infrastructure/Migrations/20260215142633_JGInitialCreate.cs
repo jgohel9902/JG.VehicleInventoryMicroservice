d9431a96ef19e6414702653fab281663ebca2fd1
Jenil Gohel <jgohel9902@gmail.com>
2026-02-15 09:27:15 -0500

    feat(infrastructure): add initial migration JGInitialCreate

---
+++ JGVehicleInventory.Infrastructure/Migrations/20260215142633_JGInitialCreate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JGVehicleInventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class JGInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JG_Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VehicleCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LocationId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    VehicleType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JG_Vehicles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JG_Vehicles_VehicleCode",
+                table: "JG_Vehicles",
+                column: "VehicleCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JG_Vehicles");
+        }
+    }
+}
